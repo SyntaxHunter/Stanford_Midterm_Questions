132c699564f3935ea000eeb91c612f5584cb2e8b
SyntaxHunter <league@WTS14.attlocal.net>
2017-07-19 11:58:59 -0700

    SummerSlam_Week2_Day3

--- src/animal_hipster/AnimalHipster.java
+++ src/animal_hipster/AnimalHipster.java
@@ -1,14 +1,40 @@
 package animal_hipster;
 
-import java.util.AbstractCollection;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class AnimalHipster {
 
-	public AbstractCollection<String> findAnimalHipsters(HashMap<String, ArrayList<String>> network,
-			HashMap<String, String> favoriteAnimals) {
-		return null;
+	public ArrayList<String> findAnimalHipsters(HashMap<String, ArrayList<String>> network,
+												HashMap<String, String> favoriteAnimals) {
+		
+		ArrayList<String> hipsters = new ArrayList<String>();
+		Object[] personsObject = network.keySet().toArray();
+		String[] persons = new String[personsObject.length];
+		
+		for(int i = 0; i < personsObject.length; i++)
+			persons[i] = (String) personsObject[i];
+		
+		boolean hipster;
+		
+		for(int i = 0; i < network.size(); i++){
+			hipster = false;
+			for(int j = 0; j < network.get(persons[i]).size(); j++){
+				if(favoriteAnimals.get(persons[i]).equals(favoriteAnimals.get(network.get(persons[i]).get(j))))
+					hipster = false;
+			}
+			if(hipster)
+				hipsters.add(persons[i]);
+		}
+		
+		for(int i = 0; i < hipsters.size(); i++)
+			System.out.print(hipsters.get(i));
+		System.out.println();
+		
+		if(hipsters.isEmpty())
+			return null;
+		else
+			return hipsters;
 	}
 
 }
--- src/oncogene/OncogeneDetector.java
+++ src/oncogene/OncogeneDetector.java
@@ -5,7 +5,52 @@ import java.util.ArrayList;
 public class OncogeneDetector {
 
 	public boolean isOncogene(ArrayList<String> healthySequences, ArrayList<String> cancerSequences, String string) {
+		if(healthySequences.isEmpty())
+			throw new IllegalArgumentException("healthySequences must contain at least one element");
 		
+		if(cancerSequences.isEmpty())
+			throw new IllegalArgumentException("cancerSequences must contain at least one element");
+		
+		if(containsInvalid(string))
+			throw new IllegalArgumentException("candidate must contain only the letters A, C, G or T");
+		
+		for(int i = 0; i < healthySequences.size(); i++){
+			if(containsInvalid(healthySequences.get(i)))
+				throw new IllegalArgumentException();
+		}
+		
+		for(int i = 0; i < cancerSequences.size(); i++){
+			if(containsInvalid(cancerSequences.get(i)))
+				throw new IllegalArgumentException();
+		}
+		
+		int healthyOccurrence = 0, cancerOccurrence = 0;
+		
+		for(int i = 0; i < healthySequences.size(); i++){
+			if(healthySequences.get(i).indexOf(string) >= 0){
+				healthyOccurrence++;
+			}
+		}
+		
+		for(int i = 0; i < cancerSequences.size(); i++){
+			if(cancerSequences.get(i).indexOf(string) >= 0){
+				cancerOccurrence++;
+			}
+		}
+		
+		if((double) cancerOccurrence/cancerSequences.size() > (double) healthyOccurrence/healthySequences.size()){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean containsInvalid(String string){
+		for(int i = 0; i < string.length(); i++){
+			if(!(string.substring(i,i+1).equals("A") || string.substring(i,i+1).equals("C") ||
+					string.substring(i,i+1).equals("T") || string.substring(i,i+1).equals("G")))
+				return true;
+		}
 		return false;
 	}
 
